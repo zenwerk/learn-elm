c996ed76faef2e5ef38114ba06817a878a8ebf25
zenwerk <zenwerk@gmail.com>
2022-01-02 00:30:23 +0900

    基本的な静的アプリ

--- jfelm-code/get-started/static-app/Picshare01.elm
+++ jfelm-code/get-started/static-app/Picshare01.elm
@@ -8,7 +8,10 @@ import Html exposing (Html, div, text)
 
 
 -- START:main
+-- Html型は仮想DOMを表す
 main : Html msg
 main =
+    -- [id, class, src, hrefなどの属性] [小要素]
+    -- htmlの文字が欲しい場合は text 関数が必要
     div [] [ text "Picshare" ]
 -- END:main
