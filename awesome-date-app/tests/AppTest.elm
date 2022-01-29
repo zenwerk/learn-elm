d07b67101e067748e08e8c0def302dad3aecfb0d
zenwerk <zenwerk@gmail.com>
2022-01-30 03:12:53 +0900

    update関数をテストする

--- awesome-date-app/tests/AppTest.elm
+++ awesome-date-app/tests/AppTest.elm
@@ -34,11 +34,13 @@ modelWithDateOffsets =
     }
 
 
+-- テスト用の SelectDate メッセージを返す関数
 selectDate : Date -> App.Msg
 selectDate date =
     App.SelectDate (Just date)
 
 
+-- テスト用の ChangeDateOffset メッセージを返す関数
 changeDateOffset : App.DateOffsetField -> Int -> App.Msg
 changeDateOffset field amount =
     App.ChangeDateOffset field (Just amount)
@@ -46,7 +48,20 @@ changeDateOffset field amount =
 
 testUpdate : Test
 testUpdate =
-    todo "implement update tests"
+    describe "update"
+        [ test "select a date" <|
+            -- 日付を選択する
+            \_ ->
+                App.update (selectDate futureDate) initialModel -- update関数に (selectDate) と Model を渡す
+                    |> Tuple.first -- (更新されたModel, Cmd) が返るのでタプルの最初を取る
+                    |> Expect.equal { initialModel | selectedDate = futureDate } -- 更新されたModelが想定されたものと同じか比較する
+        , test "change years" <|
+            -- 年を変更する
+            \_ ->
+                App.update (changeDateOffset App.Years 3) initialModel
+                    |> Tuple.first
+                    |> Expect.equal { initialModel | years = Just 3 }
+        ]
 
 
 testView : Test
